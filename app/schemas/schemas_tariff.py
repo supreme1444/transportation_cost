6000aef1164db4c0555603f2ec489b435185c1a2
supreme1444 <107640724+supreme1444@users.noreply.github.com>
2024-11-22 21:30:21 +0300

    Update schemas_tariff.py

--- app/schemas/schemas_tariff.py
+++ app/schemas/schemas_tariff.py
@@ -1,12 +1,32 @@
-from datetime import date
+from datetime import datetime
+
 from pydantic import BaseModel
 
 
 class InsuranceRequest(BaseModel):
-    date_request: date
+    date_request: datetime
     cargo_type: str
     declared_value: float
 
 
 class InsuranceResponse(BaseModel):
     cost: float
+
+
+class EditRateRequest(BaseModel):
+    id: int
+    rate: float
+    cargo_type: str
+
+
+class EditRateResponse(BaseModel):
+    cargo_type: str
+    rate: float
+
+
+class AddRateRequest(BaseModel):
+    id: int
+
+
+class AddRateRespons(BaseModel):
+    message: str
