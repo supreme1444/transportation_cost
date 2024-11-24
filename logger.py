4095135d91c234518740577a44e0063c28535a57
supreme1444 <107640724+supreme1444@users.noreply.github.com>
2024-11-24 13:40:47 +0300

    Update logger.py

--- logger.py
+++ logger.py
@@ -3,16 +3,18 @@ from datetime import datetime
 from kafka import KafkaProducer
 import json
 
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s',
-    filename='app.log',
-    filemode='a'
-)
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+file_handler = logging.FileHandler('app.log', encoding='utf-8')
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
 producer = KafkaProducer(
     bootstrap_servers='localhost:29092',
-    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+    value_serializer=lambda v: json.dumps(v, ensure_ascii=False).encode('utf-8')
+    # Убедитесь, что символы не ASCII тоже сериализуются
 )
 
 
@@ -25,8 +27,12 @@ def log_event(user_id, action, timestamp=None):
         'timestamp': timestamp
     }
     logger.info(f"Logging event: {event_message}")
-    producer.send('your_topic_name', event_message)
-    producer.flush()
+
+    try:
+        producer.send('your_topic_name', event_message)
+        producer.flush()
+    except Exception as e:
+        logger.error(f"Failed to send event: {e}")
 
 
 def close_producer():
