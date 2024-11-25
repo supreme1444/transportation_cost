7f7140493aea3c1f021bae78a18a4f20835b34cb
supreme1444 <107640724+supreme1444@users.noreply.github.com>
2024-11-25 17:53:58 +0300

    Update crud.py

--- app/crud.py
+++ app/crud.py
@@ -1,16 +1,11 @@
 from datetime import datetime
-
 from sqlalchemy.future import select
 from sqlalchemy.ext.asyncio import AsyncSession
-
 from app.models.models_tariff import Tariff
 from logger import log_event
 
 
 async def get_rate(db: AsyncSession, date: datetime, cargo_type: str):
-    """
-    Асинхронный эндпоинт для расчета стоимости страховки.
-    """
     result = await db.execute(
         select(Tariff).where(Tariff.date == date, Tariff.cargo_type == cargo_type)
     )
@@ -23,17 +18,11 @@ async def get_rate(db: AsyncSession, date: datetime, cargo_type: str):
 
 
 async def get_id_rate(db: AsyncSession, id: int):
-    """
-    Получение тарифа по ID.
-    """
     result = await db.execute(select(Tariff).filter_by(id=id))
     return result.scalar_one_or_none()
 
 
 async def edit_insurance_rate(db: AsyncSession, id: int, new_edit_rate: float, new_edit_cargo: str):
-    """
-    Асинхронная функция для редактирования тарифа.
-    """
     tariff = await get_id_rate(db, id)
     if not tariff:
         raise ValueError("Тариф не найден")
@@ -50,16 +39,14 @@ async def edit_insurance_rate(db: AsyncSession, id: int, new_edit_rate: float, n
 
 
 async def delete_insurance_rate(db: AsyncSession, id: int):
-    """
-    Асинхронная функция для удаления тарифа по его уникальному идентификатору (id).
-    """
     delete_tariff = await get_id_rate(db, id)
     if not delete_tariff:
         raise ValueError("Тариф не найден")
     log_event(
         user_id=None,
         action=f"Удаление тарифа ID {id}",
-        timestamp=datetime.utcnow())
+        timestamp=datetime.utcnow().isoformat()
+    )
     await db.delete(delete_tariff)
     await db.commit()
     return delete_tariff
@@ -68,8 +55,9 @@ async def delete_insurance_rate(db: AsyncSession, id: int):
 async def add_insurance_rate(db: AsyncSession, date: datetime, cargo: str, rate: float):
     log_event(
         user_id=None,
-        action=f"Добавление тарифа ID {date},{cargo},{rate}",
-        timestamp=datetime.utcnow())
+        action=f"Добавление тарифа на дату {date.isoformat()} для типа груза {cargo} с тарифом {rate}",
+        timestamp=datetime.utcnow().isoformat()
+    )
     new_rate = Tariff(date=date, cargo_type=cargo, rate=rate)
     db.add(new_rate)
     await db.commit()
