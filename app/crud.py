13fd31cb07590d10afa9cbce4854512bfd291b30
supreme1444 <107640724+supreme1444@users.noreply.github.com>
2024-11-25 18:28:37 +0300

    Update crud.py

--- app/crud.py
+++ app/crud.py
@@ -6,6 +6,9 @@ from logger import log_event
 
 
 async def get_rate(db: AsyncSession, date: datetime, cargo_type: str):
+    """
+    Асинхронный эндпоинт для расчета стоимости страховки.
+    """
     result = await db.execute(
         select(Tariff).where(Tariff.date == date, Tariff.cargo_type == cargo_type)
     )
@@ -17,13 +20,19 @@ async def get_rate(db: AsyncSession, date: datetime, cargo_type: str):
     return result.scalar_one_or_none()
 
 
-async def get_id_rate(db: AsyncSession, id_rate: int):
-    result = await db.execute(select(Tariff).filter_by(id=id_rate))
+async def get_id_rate(db: AsyncSession, id: int):
+    """
+    Получение тарифа по ID.
+    """
+    result = await db.execute(select(Tariff).filter_by(id=id))
     return result.scalar_one_or_none()
 
 
-async def edit_insurance_rate(db: AsyncSession, id_rate: int, new_edit_rate: float, new_edit_cargo: str):
-    tariff = await get_id_rate(db, id_rate)
+async def edit_insurance_rate(db: AsyncSession, id: int, new_edit_rate: float, new_edit_cargo: str):
+    """
+    Асинхронная функция для редактирования тарифа.
+    """
+    tariff = await get_id_rate(db, id)
     if not tariff:
         raise ValueError("Тариф не найден")
     tariff.rate = new_edit_rate
@@ -38,8 +47,11 @@ async def edit_insurance_rate(db: AsyncSession, id_rate: int, new_edit_rate: flo
     return tariff
 
 
-async def delete_insurance_rate(db: AsyncSession, id_rate: int):
-    delete_tariff = await get_id_rate(db, id_rate)
+async def delete_insurance_rate(db: AsyncSession, id: int):
+    """
+    Асинхронная функция для удаления тарифа по его уникальному идентификатору (id).
+    """
+    delete_tariff = await get_id_rate(db, id)
     if not delete_tariff:
         raise ValueError("Тариф не найден")
     log_event(
@@ -49,7 +61,7 @@ async def delete_insurance_rate(db: AsyncSession, id_rate: int):
     )
     await db.delete(delete_tariff)
     await db.commit()
-    return delete_tariff
+    return {"message": "Тариф успешно удален."}
 
 
 async def add_insurance_rate(db: AsyncSession, date: datetime, cargo: str, rate: float):
