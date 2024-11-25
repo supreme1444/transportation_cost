e69e64d122cd2743d1b4504ca407780ce7c0f509
supreme1444 <107640724+supreme1444@users.noreply.github.com>
2024-11-25 17:30:47 +0300

    Update crud.py

--- app/crud.py
+++ app/crud.py
@@ -1,18 +1,23 @@
 from datetime import datetime
+
 from sqlalchemy.future import select
 from sqlalchemy.ext.asyncio import AsyncSession
+
 from app.models.models_tariff import Tariff
 from logger import log_event
 
 
 async def get_rate(db: AsyncSession, date: datetime, cargo_type: str):
+    """
+    Асинхронный эндпоинт для расчета стоимости страховки.
+    """
     result = await db.execute(
-        select(Tariff).where(Tariff.date == date.date(), Tariff.cargo_type == cargo_type)
+        select(Tariff).where(Tariff.date == date, Tariff.cargo_type == cargo_type)
     )
     log_event(
         user_id=None,
-        action=f"Получение тарифа для типа груза '{cargo_type}' на дату {date.date().isoformat()}",
-        timestamp=datetime.utcnow().date().isoformat()
+        action=f"Получение тарифа для типа груза '{cargo_type}' на дату {date}",
+        timestamp=datetime.utcnow().isoformat()
     )
     return result.scalar_one_or_none()
 
@@ -26,6 +31,9 @@ async def get_id_rate(db: AsyncSession, id: int):
 
 
 async def edit_insurance_rate(db: AsyncSession, id: int, new_edit_rate: float, new_edit_cargo: str):
+    """
+    Асинхронная функция для редактирования тарифа.
+    """
     tariff = await get_id_rate(db, id)
     if not tariff:
         raise ValueError("Тариф не найден")
@@ -34,7 +42,7 @@ async def edit_insurance_rate(db: AsyncSession, id: int, new_edit_rate: float, n
     log_event(
         user_id=None,
         action=f"Редактирование тарифа ID {id}: новый тариф {new_edit_rate}, новый тип груза {new_edit_cargo}",
-        timestamp=datetime.utcnow().date().isoformat()
+        timestamp=datetime.utcnow().isoformat()
     )
 
     await db.commit()
@@ -42,14 +50,16 @@ async def edit_insurance_rate(db: AsyncSession, id: int, new_edit_rate: float, n
 
 
 async def delete_insurance_rate(db: AsyncSession, id: int):
+    """
+    Асинхронная функция для удаления тарифа по его уникальному идентификатору (id).
+    """
     delete_tariff = await get_id_rate(db, id)
     if not delete_tariff:
         raise ValueError("Тариф не найден")
     log_event(
         user_id=None,
         action=f"Удаление тарифа ID {id}",
-        timestamp=datetime.utcnow().date().isoformat()
-    )
+        timestamp=datetime.utcnow())
     await db.delete(delete_tariff)
     await db.commit()
     return delete_tariff
@@ -58,10 +68,9 @@ async def delete_insurance_rate(db: AsyncSession, id: int):
 async def add_insurance_rate(db: AsyncSession, date: datetime, cargo: str, rate: float):
     log_event(
         user_id=None,
-        action=f"Добавление тарифа на дату {date.date().isoformat()} для типа груза {cargo} с тарифом {rate}",
-        timestamp=datetime.utcnow().date().isoformat()
-    )
-    new_rate = Tariff(date=date.date(), cargo_type=cargo, rate=rate)
+        action=f"Добавление тарифа ID {date},{cargo},{rate}",
+        timestamp=datetime.utcnow())
+    new_rate = Tariff(date=date, cargo_type=cargo, rate=rate)
     db.add(new_rate)
     await db.commit()
     return new_rate
